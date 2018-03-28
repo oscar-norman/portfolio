782fb6621a8cfbc8a4092f53d40c8ebb24bb429f
oscar-norman <35003536+oscar-norman@users.noreply.github.com>
2018-03-28 14:44:46 +0200

    Added files

---
+++ oscar_norman/js/script.js
@@ -0,0 +1,20 @@
+function openLeft() {
+    document.getElementById("menu_left").style.width = "20%";
+    document.body.style.backgroundColor = "gray";
+}
+
+function closeLeft() {
+    document.getElementById("menu_left").style.width = "0%";
+    document.body.style.backgroundColor = "white";
+}
+
+function openRight() {
+    document.getElementById("menu_right").style.width = "20%";
+    document.body.style.backgroundColor = "gray";
+
+}
+
+function closeRight() {
+    document.getElementById("menu_right").style.width = "0%";
+    document.body.style.backgroundColor = "white";
+}
