2b9bc0fc0f41825600d28259eb9a243fede58ad0
oscar-norman <35003536+oscar-norman@users.noreply.github.com>
2018-04-18 14:59:32 +0200

    Removed cart

--- js/script.js
+++ js/script.js
@@ -7,14 +7,3 @@ function closeLeft() {
     document.getElementById("menu_left").style.width = "0%";
     document.body.style.backgroundColor = "white";
 }
-
-function openRight() {
-    document.getElementById("menu_right").style.width = "20%";
-    document.body.style.backgroundColor = "gray";
-
-}
-
-function closeRight() {
-    document.getElementById("menu_right").style.width = "0%";
-    document.body.style.backgroundColor = "white";
-}
