62a6f10f613d515204feda5f13cf360b93b47f37
oscar-norman <35003536+oscar-norman@users.noreply.github.com>
2018-04-24 23:21:09 +0200

    Delete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function openLeft() {
-    document.getElementById("menu_left").style.width = "20%";
-    document.body.style.backgroundColor = "gray";
-}
-
-function closeLeft() {
-    document.getElementById("menu_left").style.width = "0%";
-    document.body.style.backgroundColor = "white";
-}
