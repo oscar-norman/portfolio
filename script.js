e80785c5e05cbfe330f3f144b5be7e8bbc3cef91
oscar-norman <35003536+oscar-norman@users.noreply.github.com>
2018-04-26 01:02:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-var src = document.getElementsById("").getAttribut("src");
+
+function changeStartImage(src) {
+	document.getElementById("start-image").setAttribute("src", src);
+	document.getElementById("start-image").scrollIntoView({ block: 'end',  behavior: 'smooth' });
+}
+
+function loadImages() {
+	for (var i = 1; i <= 37; i++) {
+		var html = '<div class="gallery-image"><img src="bilder/bild_'+i+'.jpg" id="bild_'+i+'" onclick="changeStartImage(this.src)"></div>'
+		document.getElementById("gallery-wrapper").innerHTML += html;
+	}
+}
