a6bf3d8cd3d7465ef0c362384ba569269295b769
oscar-norman <35003536+oscar-norman@users.noreply.github.com>
2018-05-02 15:01:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function loadImages() {
 		document.getElementById("gallery-wrapper").innerHTML += html;
 	}
 }
+
+$(document).ready(function() {
+  $('#contact-form').submit(function(e) {
+    var name    = document.getElementById('inputName')
+    var email   = document.getElementById('inputEmail')
+    var message = document.getElementById('inputMessage')
+
+    if (!name.value || !email.value || !message.value) {
+      alertify.error("Please check your entries");
+      return false;
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '//formspree.io/oscar-norman@hotmail.com',
+        data: $('#contact-form').serialize(),
+        datatype: 'json'
+      });
+      e.preventDefault();
+      $(this).get(0).reset();
+      alertify.success("Message sent");
+    }
+  });
+});
